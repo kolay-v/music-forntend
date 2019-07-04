f65457112e1ee8401a42193469252ff09b3126fe
@kolayv <kolayv@pm.me>
2019-07-04 01:11:57 +0000

    fixed travis and updated readme
    Signed-off-by: @kolayv <kolayv@pm.me>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,8 @@
 module.exports = {
     "env": {
         "browser": true,
-        "es6": true
+        "es6": true,
+        "jest": true
     },
     "parser": "babel-eslint",
     "extends": ["standard-react", "standard"],
