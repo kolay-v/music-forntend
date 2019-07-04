e0659e354f94cc154795d503f04332fc0b67d835
@kolayv <kolayv@pm.me>
2019-07-04 01:24:47 +0000

    now use yarn in travis and fixed tests
    Signed-off-by: @kolayv <kolayv@pm.me>

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -2,17 +2,21 @@ import React from 'react'
 import Search from '../../routes/search'
 import TrackBar from '../track-bar'
 import { version } from '../../package.alias.json'
+import store from '../../utils/store'
+import { Provider } from 'react-redux'
 import './app.css'
 
 function App () {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <TrackBar />
-        <Search />
-        <p>Version {version}</p>
-      </header>
-    </div>
+    <Provider store={store}>
+      <div className='App'>
+        <header className='App-header'>
+          <TrackBar />
+          <Search />
+          <p>Version {version}</p>
+        </header>
+      </div>
+    </Provider>
   )
 }
 
--- src/index.js
+++ src/index.js
@@ -2,12 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './components/app'
 import * as serviceWorker from './serviceWorker'
-import store from './utils/store'
-import { Provider } from 'react-redux'
 
-ReactDOM.render(<Provider store={store}>
-  <App />
-</Provider>, document.getElementById('root'))
+ReactDOM.render(<App />, document.getElementById('root'))
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
